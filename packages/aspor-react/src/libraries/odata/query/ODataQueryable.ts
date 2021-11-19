4702e94f6589cd6bd62d540a27368637fc33bdb8
Davide Wietlisbach <davide.wietlisbach@icloud.com>
2021-11-19 21:11:31 +0100

    Fixed an issue in getFirst

--- packages/aspor-react/src/libraries/odata/query/ODataQueryable.ts
+++ packages/aspor-react/src/libraries/odata/query/ODataQueryable.ts
@@ -125,14 +125,21 @@ export default class ODataQueryable<Entity, UEntity = Entity> extends AbstractOD
             let query : ODataQuerySegments = {}
             ODataExpressionVisitor.visit(query,this._expression);
             query.top = 1;
-            return this._base.client().get(this.url()+ODataQueryUtility.compileQuery(query));
+            return new Promise((resolve,reject)=>{
+                this._base.client().get(this.url()+ODataQueryUtility.compileQuery(query))
+                    .then((result : any)=>{
+                        if(result.value && result.value.length > 0) return result.value[0];
+                        else reject();
+                    }).catch(reject)
+            });
+        }else{
+            return new Promise((resolve,reject)=>{
+                this._base.client().get(this.url()+"?$top=1")
+                    .then((result : any)=>{
+                        if(result.value && result.value.length > 0) return result.value[0];
+                        else reject();
+                    }).catch(reject)
+            });
         }
-        return new Promise((resolve,reject)=>{
-            this._base.client().get(this.url()+"?$top=1")
-                .then((result : any)=>{
-                    if(result.value && result.value.length > 0) return result.value[0];
-                    else reject();
-                }).catch(reject)
-        });
     }
 }
